6fcc4627271cd9dc742781a94dc7f16a178d748c
Bill Feng <bill.feng@yale.edu>
2021-05-23 00:58:50 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,9 +2,9 @@ def find_max_value(array)
   # Add your solution here
   max=0
   for element in array {
-    if (element>max){
+    if (element>max)
       max=element
-    }
+    end
   }
   max
 end
